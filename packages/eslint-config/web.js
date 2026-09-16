2d8fb0731bb627997e77a9789cb4912a971cad48
huanngdev <huanngdev@gmail.com>
2026-09-16 18:05:07 +0700

    chore(web): configure frontend tooling

--- packages/eslint-config/web.js
+++ packages/eslint-config/web.js
@@ -1,3 +1,4 @@
+import { plugin as shadcn } from "@shadcn/lint";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
@@ -7,9 +8,15 @@ const webConfig = [
   ...baseConfig,
   {
     files: ["**/*.{jsx,tsx}"],
+    settings: {
+      shadcn: {
+        ui: "@/components/ui",
+      },
+    },
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      shadcn,
     },
     rules: {
       "react-hooks/exhaustive-deps": "warn",
@@ -20,6 +27,21 @@ const webConfig = [
           allowConstantExport: true,
         },
       ],
+      "shadcn/no-arbitrary-values": "error",
+      "shadcn/no-inline-styles": "error",
+      "shadcn/no-raw-colors": "error",
+      "shadcn/no-restyle": ["error", { allow: ["layout"] }],
+      "shadcn/no-unknown-classes": "error",
+      "shadcn/require-static-classes": "error",
+    },
+  },
+  {
+    files: ["**/components/ui/**/*.{jsx,tsx}"],
+    rules: {
+      "shadcn/no-arbitrary-values": "off",
+      "shadcn/no-restyle": "off",
+      "shadcn/require-static-classes": "off",
+      "react-refresh/only-export-components": "off",
     },
   },
 ];
